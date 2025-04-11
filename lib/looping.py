00dc95ec10a60f9b8c12ee858fa7df4e88bb5e51
dennis mukuria <denismukuria@gmail.com>
2025-04-11 09:29:00 +0300

    complete all tests

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
-
+    n = 10
+    while n > 0:
+        print(n)
+        n -= 1
+    else: print('Happy New Year!')
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [n**2 for n in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    n = 1
+    while n < 101:
+        if n % 3 == 0 and n % 5 == 0:
+            print('FizzBuzz')
+        elif n % 3 == 0:
+            print('Fizz')
+        elif n % 5 == 0:
+            print('Buzz')
+        else: print(n)
+        n += 1
+
